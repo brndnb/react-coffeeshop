70965914b1a4b9e606b52fb076311497ac6295f5
brndnb <116584527+brndnb@users.noreply.github.com>
2023-01-28 20:36:17 -0500

    API fetch and sttes done confirmed working. No styling

---
+++ src/CoffeeProfile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function CoffeeProfile(props) {
+  return (
+    <div>
+        <article className='Main-box'>
+            <h2>{props.profile.title}</h2>
+            <img src={props.profile.imageUrl} className="profile-image"></img>
+            <p>{props.profile.description}</p>
+            <p>{props.profile.ingredients}</p>
+        </article>
+    </div>
+  )
+}
+
+export default CoffeeProfile
--- src/Main.js
+++ src/Main.js
@@ -1,28 +1,84 @@
 import React from 'react'
+import CoffeeProfile from './CoffeeProfile'
 
 function Main() {
+    //Create States for Coffee Profiles
+
+    const [profileone, setProfileOne] = React.useState({
+        title:"",
+        imageUrl:"",
+        description:"",
+        ingredients:""
+    })
+    const [profiletwo, setProfileTwo] = React.useState({
+        title:"",
+        imageUrl:"",
+        description:"",
+        ingredients:""
+    })
+    const [profilethree, setProfileThree] = React.useState({
+        title:"",
+        imageUrl:"",
+        description:"",
+        ingredients:""
+    })
+    const [profilefour, setProfileFour] = React.useState({
+        title:"",
+        imageUrl:"",
+        description:"",
+        ingredients:""
+    })
+
+
+    //Use Effect
+
+    React.useEffect(
+
+    () => {
+
+    //API fetch call
+
+    fetch("https://api.sampleapis.com/coffee/hot")
+    .then(response => response.json())
+    .then (response => {
+
+    
+        //Save data to state
+        setProfileOne({...profileone,                
+                          title:response[0].title,
+                          imageUrl:response[0].image,
+                          description:response[0].description,
+                          ingredients:response[0].ingredients})
+        setProfileTwo({...profiletwo,
+                          title:response[1].title,
+                          imageUrl:response[1].image,
+                          description:response[1].description,
+                          ingredients:response[1].ingredients})
+        setProfileThree({...profilethree,
+                          title:response[2].title,
+                          imageUrl:response[2].image,
+                          description:response[2].description,
+                          ingredients:response[2].ingredients})
+        setProfileFour({...profilefour,
+                          title:response[3].title,
+                          imageUrl:response[3].image,
+                          description:response[3].description,
+                          ingredients:response[3].ingredients})
+        })
+    .catch(error => console.log(error))
+
+
+
+     },
+    )
+
+
   return (
     <div className='Main-container'>
-        <article>
-            <div className='Main-box'></div>
-            <h2>Coffee #1</h2>
-            <p>Description of coffee</p>
-        </article>
-        <article>
-            <div className='Main-box'></div>
-            <h2>Coffee #2</h2>
-            <p>Description of coffee</p>
-        </article>
-        <article>
-            <div className='Main-box'></div>
-            <h2>Coffee #3</h2>
-            <p>Description of coffee</p>
-        </article>
-        <article>
-            <div className='Main-box'></div>
-            <h2>Coffee #4</h2>
-            <p>Description of coffee</p>
-        </article>
+       <CoffeeProfile profile={profileone}/>
+       <CoffeeProfile profile={profiletwo}/>
+       <CoffeeProfile profile={profilethree} />
+       <CoffeeProfile profile={profilefour} />
     </div>
   )
 }
